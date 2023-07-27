a2bef18fe9ea6503e08dabb7684335f1611bf516
Python Dev <vitaliitolokov@gmail.com>
2023-07-27 14:59:26 +0500

    created first model

--- multipleTransactions/demoApp/models.py
+++ multipleTransactions/demoApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """It stores the client's personal information and the amount of money"""
+
+    full_name = models.CharField("Имя пользователя", max_length=150)
+    inn = models.CharField("ИНН", max_length=12, unique=True)
+    wallet = models.DecimalField("Кошелёк", max_digits=12, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'{self.full_name}'
+
+    class Meta:
+        unique_together = (('full_name', 'inn'),)
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
