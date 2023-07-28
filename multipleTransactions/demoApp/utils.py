a292df7c385eb2c335a7d5ffd0362e1a8fe11dca
Python Dev <vitaliitolokov@gmail.com>
2023-07-28 20:51:18 +0500

    updated Transaction class

--- multipleTransactions/demoApp/utils.py
+++ multipleTransactions/demoApp/utils.py
@@ -1,4 +1,8 @@
+from django.db.models import F
+
 from decimal import Decimal, ROUND_DOWN
+
+from .models import Profile
 
 
 class Transaction:
@@ -10,13 +14,16 @@ class Transaction:
         self.count_recipients = self.inns_list.__len__()
         self.money = Decimal(money_to_be_debited)
 
-    # Списание средств
     def run(self):
         #
         try:
             up_wallet = self.calculate_average()
             down_wallet = self.calculate_subtraction(up_wallet)
-            print(up_wallet, down_wallet, self.count_recipients)
+
+            if self.up_wallet_in_model(up_wallet) and self.down_wallet(down_wallet):
+                print("Транзакция проведена успешно")
+                print(f"Списано {down_wallet} по {up_wallet} с {self.count_recipients} пользователей")
+
         except Exception as e:
             print("Транзакция отменена: ", e)
         finally:
@@ -24,7 +31,7 @@ class Transaction:
 
     def calculate_average(self):
         """Высчитывает, сколько должен средств должен получить каждый пользователь"""
-        item = Decimal(self.money/self.count_recipients).quantize(Decimal('0.00'), rounding=ROUND_DOWN)
+        item = Decimal(self.money / self.count_recipients).quantize(Decimal('0.00'), rounding=ROUND_DOWN)
         return item
 
     def calculate_subtraction(self, up_wallet):
@@ -32,6 +39,15 @@ class Transaction:
         item = up_wallet * self.count_recipients
         return item
 
+    def up_wallet_in_model(self, up_wallet):
+        """Увеличение значения кошелька пользователей с перечисленными ИНН"""
+        Profile.objects.filter(inn__in=self.inns_list).update(wallet=F("wallet") + up_wallet)
+        return True
+
+    def down_wallet(self, down_wallet):
+        """Уменьшение значения кошелька конкретного пользователя"""
+        Profile.objects.filter(pk=self.payer.pk).update(wallet=F("wallet") - down_wallet)
+        return True
 
     def __str__(self):
         return str(f"{self.payer, self.inns_list, self.count_recipients, self.money}")
