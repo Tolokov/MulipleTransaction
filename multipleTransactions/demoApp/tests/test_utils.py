9b5abfec767fd36c95cc03a5fb532a6c8d89b092
Python Dev <vitaliitolokov@gmail.com>
2023-08-03 13:10:40 +0500

    test utils

--- multipleTransactions/demoApp/tests/test_utils.py
+++ multipleTransactions/demoApp/tests/test_utils.py
@@ -23,7 +23,7 @@ class UtilsTestCase(TestCase):
         self.five = Profile.objects.create(user=user_five, full_name="5", inn="505", wallet=Decimal("100.0"))
 
     def test_init(self):
-        t = Transaction(payer="3", inns="101,102", money_to_be_debited=50.00)
+        t = Transaction(payer="3", inn="303", inns="101,102", money_to_be_debited=50.00)
         self.assertEqual(t.payer, "3")
         self.assertEqual(t.count_recipients, 2)
         self.assertEqual(t.money, 50.00)
@@ -39,15 +39,17 @@ class UtilsTestCase(TestCase):
             (",0312324444123456789098,", 1),
         )
         for num, i in enumerate(cases):
-            t = Transaction(payer="2", inns=i[0], money_to_be_debited=0.00)
+            t = Transaction(payer="2", inn="202", inns=i[0], money_to_be_debited=0.00)
             self.assertEqual(t.count_recipients, i[1], msg=f"{num}")
 
     def test_string(self):
-        t = Transaction(payer=self.one, inns="0,23" * 3, money_to_be_debited=9999999)
-        self.assertEqual(t.__str__(), "(<Profile: ('1', '101')>, ['0', '230', '230', '23'], 4, Decimal('9999999'))")
+        t = Transaction(payer=self.one, inn="101", inns="0,23" * 3, money_to_be_debited=9999999)
+        self.assertEqual(t.__str__(),
+                         "(<Profile: ('1', '101')>, '101', ['0', '230', '230', '23'], 4, Decimal('9999999'))")
 
+    #
     def test_calculate_subtraction(self):
-        t = Transaction(payer=self.two, inns="0,23", money_to_be_debited=9.99)
+        t = Transaction(payer=self.two, inn="202", inns="0,23", money_to_be_debited=9.99)
         item = t.calculate_subtraction(up_wallet=2)
         self.assertEqual(item, 4)
 
@@ -65,7 +67,7 @@ class UtilsTestCase(TestCase):
             ("J," * 9999, 99.02, 990100.98),
         )
         for num, i in enumerate(cases):
-            t = Transaction(payer="None", inns=i[0], money_to_be_debited=0)
+            t = Transaction(payer="None", inn="606", inns=i[0], money_to_be_debited=0)
             item = t.calculate_subtraction(up_wallet=i[1])
             self.assertEqual(item, i[2], msg=f"{num}")
 
@@ -86,12 +88,12 @@ class UtilsTestCase(TestCase):
             ("101, 100, 99", "99999999.999", Decimal("33333333.33")),
         )
         for num, i in enumerate(cases):
-            t = Transaction(payer="2", inns=i[0], money_to_be_debited=i[1])
+            t = Transaction(payer="2", inn="202", inns=i[0], money_to_be_debited=i[1])
             item = t.calculate_average()
             self.assertEqual(item, i[2], msg=f"{num}")
 
     def test_run_successful_transaction(self):
-        transaction = Transaction(self.four, "101,202,303", 30)
+        transaction = Transaction(self.four, "404", "101,202,303", 30)
         transaction.run()
         self.assertEqual(Profile.objects.get(pk=self.four.pk).wallet, 70)
         self.assertEqual(Profile.objects.get(inn='101').wallet, 10)
@@ -99,16 +101,16 @@ class UtilsTestCase(TestCase):
         self.assertEqual(Profile.objects.get(inn='303').wallet, 10)
 
         # write-off from oneself
-        transaction = Transaction(self.four, "404", 30)
+        transaction = Transaction(self.four, "404", "404", 30)
         transaction.run()
         self.assertEqual(Profile.objects.get(pk=self.four.pk).wallet, 70)
 
-        transaction = Transaction(self.four, "505", 70)
+        transaction = Transaction(self.four, "404", "505", 70)
         transaction.run()
         self.assertEqual(Profile.objects.get(pk=self.four.pk).wallet, 0)
         self.assertEqual(Profile.objects.get(pk=self.five.pk).wallet, 170)
 
-        transaction = Transaction(self.five, "101,202,303,404,505", 170)
+        transaction = Transaction(self.five, "505", "101,202,303,404,505", 170)
         transaction.run()
         self.assertEqual(Profile.objects.get(inn='101').wallet, 44)
         self.assertEqual(Profile.objects.get(inn='202').wallet, 44)
@@ -117,7 +119,7 @@ class UtilsTestCase(TestCase):
         self.assertEqual(Profile.objects.get(inn='505').wallet, 34)
 
     def test_run_failed_transaction(self):
-        transaction = Transaction(self.four, "101,202,303", 100)
+        transaction = Transaction(self.four, "404", "101,202,303", 100)
         transaction.run()
 
         # transaction canceled
